b683203952bf818d7d9fb3e1ea72c23ccaa742f3
Haithamsaleh <alawaji.haitham@gmail.com>
2022-08-08 12:33:30 +0300

    updata

--- app/src/main/java/com/example/vatapp/MainActivity.kt
+++ app/src/main/java/com/example/vatapp/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.vatapp
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
 import com.example.vatapp.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,12 +23,41 @@ class MainActivity : AppCompatActivity() {
             calculateVat()
         }
 
-
-
-
     }
 
+    @SuppressLint("StringFormatInvalid")
     private fun calculateVat() {
-        TODO("Not yet implemented")
+        val stringInTextField = binding.etTotal.text.toString()
+        val cost = stringInTextField.toDouble()
+
+        val selected = binding.percent.checkedRadioButtonId
+        val vatPercentage = when (selected){
+
+            R.id.percent_10 -> 0.10
+            R.id.percent_15 -> 0.15
+            else -> 0.20
+        }
+
+
+        var vat =  vatPercentage * cost
+        var total =  cost + vat
+
+        val roundVat = binding.switchRoundUp.isChecked
+
+        if (roundVat){
+            total = kotlin.math.ceil(total)
+        }
+
+        val formattedTotal = NumberFormat.getCurrencyInstance().format(total)
+       binding.txtTotal.text = getString(R.string.totals,formattedTotal)
+        val formattedVat = NumberFormat.getCurrencyInstance().format(vat)
+
+        binding.txtVatTotal.text = getString(R.string.vats, formattedVat)
+        val formattedCost = NumberFormat.getCurrencyInstance().format(cost)
+
+        binding.txtCostTotal.text = getString(R.string.cost, formattedCost)
+
     }
-}
+
+
+    }
